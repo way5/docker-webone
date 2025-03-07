13682e22b86dba747905fc218162868a43faca0d
Sergey Ko <u306060@gmail.com>
2025-03-07 00:06:07 -0600

    webone 0.17.4, environment variables, README update, alpine 3.21, dotnet 8.0, cleanup

--- include/entry.sh
+++ include/entry.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Run /usr/local/webone/webone with CONFIG_PATH if it is set
-if [ -n "$CONFIG_PATH" ]; then
-    exec /usr/local/webone/webone "$CONFIG_PATH"
+if [ -n "$CONFIG_PATH" ] && [ -f "$CONFIG_PATH" ]; then
+    exec /usr/local/webone/webone $CONFIG_PATH
 else
-    exec /usr/local/webone/webone
+    exec /usr/local/webone/webone /etc/webone_defaults.conf
 fi
